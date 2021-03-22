0e082442dccc9ac42973d29de26eb9d4d05af28e
Daniel Hu <hdmdhr@gmail.com>
2021-03-22 11:28:50 -0600

    demo just, of, from, empty(), error()
    - Add util example method

---
+++ Presentation-MVVM with RxSwift/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  Presentation-MVVM with RxSwift
+//
+//  Created by 胡洞明 on 2021-03-22.
+//
+
+import Foundation
+
+public func example(of description: String, action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
+}
--- Presentation-MVVM with RxSwift/ViewController.swift
+++ Presentation-MVVM with RxSwift/ViewController.swift
@@ -10,13 +10,25 @@ import RxSwift
 
 class ViewController: UIViewController {
 
-    let o = PublishSubject<Void>()
+    let subject = PublishSubject<Void>()
+    var o1: Observable<Int>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // demo Event enum
+        subject.on(Event<Void>.completed)
         
-        o.on(Event<Void>.completed)
+        // demo just, of, from
+        o1 = Observable.just(1)
+        o1 = Observable.of(1, 2)
+        o1 = Observable.from([1, 2, 3])
+        
+        // demo empty()
+        o1 = .empty()
+        
+        // demo error()
+        o1 = .error(NSError(domain: "", code: 0))
     }
 
 
