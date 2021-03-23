1c6aa5cd48b98315356a01493014e0a24f3bc33a
Daniel Hu <hdmdhr@gmail.com>
2021-03-23 14:46:46 -0600

    Add FavDateSpotsVC (storyboard and code)

---
+++ Presentation-MVVM with RxSwift/ViewController/FavDateSpotsVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavDateSpotsVC.swift
+//  Presentation-MVVM with RxSwift
+//
+//  Created by 胡洞明 on 2021-03-23.
+//
+
+import UIKit
+
+class FavDateSpotsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
